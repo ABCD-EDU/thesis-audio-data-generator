2fd105a118e07ba262f8f841a19f692b7ef9811d
Jaime Fernandez Jr <jaimefernandez551@gmail.com>
2023-02-07 13:09:02 +0800

    modify: made running bats in run_batch_files linear

--- scripts/run_batch_files.bat
+++ scripts/run_batch_files.bat
@@ -1,14 +1,14 @@
 @echo off
 
-start download_n_extract_data.bat 
+call download_n_extract_data.bat 
 
-start generate_librispeech_metadata.bat
+call generate_librispeech_metadata.bat
 
-start generate_wham_metadata.bat
+call generate_wham_metadata.bat
 
-start generate_librimix_metadata.bat
+call generate_librimix_metadata.bat
 
-start generate_librimix_from_metadata.bat
+call generate_librimix_from_metadata.bat
 
 echo All bat files have completed.
 
